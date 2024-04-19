3fa1f612024beb2f3e9bd93a1b36048824c8cb72
mrinsaf <mr.insaf@list.ru>
2024-04-19 14:51:05 +0300

    Добавил общение с бэкендом

--- app/src/main/java/com/example/mynfc/MainActivity.kt
+++ app/src/main/java/com/example/mynfc/MainActivity.kt
@@ -8,23 +8,33 @@ import android.content.IntentFilter.MalformedMimeTypeException
 import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.nfc.tech.MifareClassic
+import android.os.Build.VERSION_CODES.Q
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -39,30 +49,57 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+
 import com.example.mynfc.ui.theme.MyNFCTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import okhttp3.Call
 import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
+import org.json.JSONObject
 import java.io.IOException
 import java.math.BigInteger
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+import kotlin.coroutines.EmptyCoroutineContext
 
 var balance by mutableStateOf("")
 var name by mutableStateOf("")
 var isAddingBalance by  mutableStateOf(false)
 var newBalance by mutableStateOf("0")
+var debugMessage by mutableStateOf("Начальное сообщение")
 
 class MainActivity : ComponentActivity() {
 
@@ -117,11 +154,13 @@ class MainActivity : ComponentActivity() {
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
-        resolveIntent(intent!!, isAddingBalance)
-
+        val scope = CoroutineScope(EmptyCoroutineContext)
+        val job = scope.launch {
+            resolveIntent(intent!!, isAddingBalance)
+        }
     }
 
-    private fun resolveIntent(intent: Intent, writeBalance: Boolean) {
+    private suspend fun resolveIntent(intent: Intent, writeBalance: Boolean) = coroutineScope {
         val action = intent.action
         if (NfcAdapter.ACTION_TECH_DISCOVERED == action) {
             val tagFromIntent = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
@@ -129,19 +168,31 @@ class MainActivity : ComponentActivity() {
                 writeBalance(tagFromIntent)
             }
             else {
-                readData(tagFromIntent)
+                try {
+                    readData(tagFromIntent)
+                }
+                catch (e: Exception) {
+                    println(e)
+                    debugMessage += "\n $e"
+                }
             }
         }
 
     }
 
-    private fun writeBalance(tag: Tag?) {
+    private suspend fun writeBalance(tag: Tag?) = coroutineScope {
         val mfc = MifareClassic.get(tag)
+        val readArray = readData(tag)
+
+        val cardId = readArray[0]
+        val sector10Key = readArray[1]
+        val sector12Key = readArray[2]
+
         for (i in 0..1) {
             val bIndex = mfc.sectorToBlock(12) + i
             try {
                 mfc.connect()
-                val auth = mfc.authenticateSectorWithKeyA(12, SectorKeys.SECTOR12)
+                val auth = mfc.authenticateSectorWithKeyA(12, sector12Key)
                 if (auth) {
                     var floatBalance = newBalance.toFloat()
                     floatBalance *= 100
@@ -164,25 +215,36 @@ class MainActivity : ComponentActivity() {
                         }
                     } catch (e: Exception) {
                         println("Error writing block: $e")
+                        debugMessage += "\n $e"
                     }
                 } else {
                     println("No auth for writing")
                 }
             } catch (e: Exception) {
                 println("Error connecting to tag: $e")
+                debugMessage += "\n $e"
             } finally {
                 try {
                     mfc?.close()
                 } catch (e: IOException) {
                     println("Error closing tag: $e")
+                    debugMessage += "\n $e"
                 }
             }
         }
 
     }
 
+    private fun hexStringToByteArray(hex: String): ByteArray {
+        val byteArray = ByteArray(hex.length / 2)
+        for (i in hex.indices step 2) {
+            val byte = hex.substring(i, i + 2).toInt(16).toByte()
+            byteArray[i / 2] = byte
+        }
+        return byteArray
+    }
 
-    private fun readData(tag: Tag?) {
+    private suspend fun readData(tag: Tag?) = coroutineScope {
         val mfc = MifareClassic.get(tag)
         mfc.connect()
         val cardId = tag?.id
@@ -194,13 +256,24 @@ class MainActivity : ComponentActivity() {
         var bIndex: Int
 
         println("card id: ${ getHexString(cardId, cardId?.size ?: 0) }")
-
         println("sector count: $secCount")
+
+        val response: Deferred<Array<String>> = async {
+            debugMessage += "\nПолучаю ключи"
+            get_keys(cardId)
+        }
+        val keysArray = response.await()
+
+        val sector10Key = hexStringToByteArray(keysArray[0])
+        val sector12Key = hexStringToByteArray(keysArray[1])
+        debugMessage += "\nПолучил ключи " +
+                "${ getHexString(sector10Key, sector10Key?.size ?: 0) }, " +
+                getHexString(sector12Key, sector12Key?.size ?: 0)
 
         for (j in 0 until secCount) {
             val authKey = when(j) {
-                10 -> SectorKeys.SECTOR10
-                12 -> SectorKeys.SECTOR12
+                10 -> sector10Key
+                12 -> sector12Key
                 else -> MifareClassic.KEY_DEFAULT
             }
             authA = mfc.authenticateSectorWithKeyA(j, authKey)
@@ -229,7 +302,34 @@ class MainActivity : ComponentActivity() {
                 println("sector $j: Auth failed")
             }
         }
+        mfc.close()
+        return@coroutineScope arrayOf(cardId, sector10Key, sector12Key)
     }
+
+    suspend fun get_keys(cardId: ByteArray?): Array<String> = coroutineScope {
+        val url = "http://188.120.254.122:8000/nfc/hello/"
+        val client = OkHttpClient()
+        val jsonBody = """
+                    {
+                        "cardId": "${getHexString(cardId, cardId?.size ?: 0)}"
+                    }
+                """.trimIndent()
+        val mediaType = "application/json; charset=utf-8".toMediaType()
+        val requestBody = jsonBody.toRequestBody(mediaType)
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+        val response = client.newCall(request).execute()
+        val jsonResponse = response.body?.string() ?: ""
+        val jsonObject = JSONObject(jsonResponse)
+
+
+        val sector10Key = jsonObject.getString("sector_10_key")
+        val sector12Key = jsonObject.getString("sector_12_key")
+        return@coroutineScope arrayOf(sector10Key, sector12Key)
+    }
+
 
     fun intToBytes(value: Int): ByteArray {
         val result = ByteArray(4)
