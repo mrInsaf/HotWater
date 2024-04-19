7e654337c92411548b298c7582148f3f214a5bbc
mrinsaf <mr.insaf@list.ru>
2024-04-19 14:48:41 +0300

    Добавил много ништяков

--- app/src/main/java/com/example/mynfc/MainActivity.kt
+++ app/src/main/java/com/example/mynfc/MainActivity.kt
@@ -8,23 +8,33 @@ import android.content.IntentFilter.MalformedMimeTypeException
 import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.nfc.tech.MifareClassic
+import android.os.Build.VERSION_CODES.Q
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -39,30 +49,59 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+
 import com.example.mynfc.ui.theme.MyNFCTheme
+import com.example.mynfc.ui.theme.paddingStart
+import com.example.mynfc.ui.theme.paddingTop
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import okhttp3.Call
 import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
+import org.json.JSONObject
 import java.io.IOException
 import java.math.BigInteger
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+import kotlin.coroutines.EmptyCoroutineContext
 
 var balance by mutableStateOf("")
 var name by mutableStateOf("")
 var isAddingBalance by  mutableStateOf(false)
 var newBalance by mutableStateOf("0")
+var debugMessage by mutableStateOf("Начальное сообщение")
 
 class MainActivity : ComponentActivity() {
 
@@ -117,11 +156,13 @@ class MainActivity : ComponentActivity() {
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
-        resolveIntent(intent!!, isAddingBalance)
-
+        val scope = CoroutineScope(EmptyCoroutineContext)
+        val job = scope.launch {
+            resolveIntent(intent!!, isAddingBalance)
+        }
     }
 
-    private fun resolveIntent(intent: Intent, writeBalance: Boolean) {
+    private suspend fun resolveIntent(intent: Intent, writeBalance: Boolean) = coroutineScope {
         val action = intent.action
         if (NfcAdapter.ACTION_TECH_DISCOVERED == action) {
             val tagFromIntent = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
@@ -129,19 +170,31 @@ class MainActivity : ComponentActivity() {
                 writeBalance(tagFromIntent)
             }
             else {
-                readData(tagFromIntent)
+                try {
+                    readData(tagFromIntent)
+                }
+                catch (e: Exception) {
+                    println(e)
+                    debugMessage += "\n $e"
+                }
             }
         }
 
     }
 
-    private fun writeBalance(tag: Tag?) {
+    private suspend fun writeBalance(tag: Tag?) = coroutineScope {
         val mfc = MifareClassic.get(tag)
+        val readArray = readData(tag)
+
+        val cardId = readArray[0]
+        val sector10Key = readArray[1]
+        val sector12Key = readArray[2]
+
         for (i in 0..1) {
             val bIndex = mfc.sectorToBlock(12) + i
             try {
                 mfc.connect()
-                val auth = mfc.authenticateSectorWithKeyA(12, SectorKeys.SECTOR12)
+                val auth = mfc.authenticateSectorWithKeyA(12, sector12Key)
                 if (auth) {
                     var floatBalance = newBalance.toFloat()
                     floatBalance *= 100
@@ -164,25 +217,36 @@ class MainActivity : ComponentActivity() {
                         }
                     } catch (e: Exception) {
                         println("Error writing block: $e")
+                        debugMessage += "\n $e"
                     }
                 } else {
                     println("No auth for writing")
                 }
             } catch (e: Exception) {
                 println("Error connecting to tag: $e")
+                debugMessage += "\n $e"
             } finally {
                 try {
                     mfc?.close()
                 } catch (e: IOException) {
                     println("Error closing tag: $e")
+                    debugMessage += "\n $e"
                 }
             }
         }
 
     }
 
+    private fun hexStringToByteArray(hex: String): ByteArray {
+        val byteArray = ByteArray(hex.length / 2)
+        for (i in hex.indices step 2) {
+            val byte = hex.substring(i, i + 2).toInt(16).toByte()
+            byteArray[i / 2] = byte
+        }
+        return byteArray
+    }
 
-    private fun readData(tag: Tag?) {
+    private suspend fun readData(tag: Tag?) = coroutineScope {
         val mfc = MifareClassic.get(tag)
         mfc.connect()
         val cardId = tag?.id
@@ -194,13 +258,24 @@ class MainActivity : ComponentActivity() {
         var bIndex: Int
 
         println("card id: ${ getHexString(cardId, cardId?.size ?: 0) }")
-
         println("sector count: $secCount")
+
+        val response: Deferred<Array<String>> = async {
+            debugMessage += "\nПолучаю ключи"
+            get_keys(cardId)
+        }
+        val keysArray = response.await()
+
+        val sector10Key = hexStringToByteArray(keysArray[0])
+        val sector12Key = hexStringToByteArray(keysArray[1])
+        debugMessage += "\nПолучил ключи " +
+                "${ getHexString(sector10Key, sector10Key?.size ?: 0) }, " +
+                getHexString(sector12Key, sector12Key?.size ?: 0)
 
         for (j in 0 until secCount) {
             val authKey = when(j) {
-                10 -> SectorKeys.SECTOR10
-                12 -> SectorKeys.SECTOR12
+                10 -> sector10Key
+                12 -> sector12Key
                 else -> MifareClassic.KEY_DEFAULT
             }
             authA = mfc.authenticateSectorWithKeyA(j, authKey)
@@ -229,7 +304,34 @@ class MainActivity : ComponentActivity() {
                 println("sector $j: Auth failed")
             }
         }
+        mfc.close()
+        return@coroutineScope arrayOf(cardId, sector10Key, sector12Key)
     }
+
+    suspend fun get_keys(cardId: ByteArray?): Array<String> = coroutineScope {
+        val url = "http://188.120.254.122:8000/nfc/hello/"
+        val client = OkHttpClient()
+        val jsonBody = """
+                    {
+                        "cardId": "${getHexString(cardId, cardId?.size ?: 0)}"
+                    }
+                """.trimIndent()
+        val mediaType = "application/json; charset=utf-8".toMediaType()
+        val requestBody = jsonBody.toRequestBody(mediaType)
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+        val response = client.newCall(request).execute()
+        val jsonResponse = response.body?.string() ?: ""
+        val jsonObject = JSONObject(jsonResponse)
+
+
+        val sector10Key = jsonObject.getString("sector_10_key")
+        val sector12Key = jsonObject.getString("sector_12_key")
+        return@coroutineScope arrayOf(sector10Key, sector12Key)
+    }
+
 
     fun intToBytes(value: Int): ByteArray {
         val result = ByteArray(4)
@@ -343,6 +445,10 @@ fun MainPage(modifier: Modifier = Modifier) {
                     fontSize = 24.sp,
                     modifier = modifier
                 )
+                Text(
+                    text = debugMessage,
+                    color = colorResource(id = R.color.color1),
+                )
             }
             Spacer(modifier = modifier.size(48.dp))
         }
@@ -383,17 +489,184 @@ fun MainPage(modifier: Modifier = Modifier) {
 }
 
 
-@Preview
+//@Preview
+//@Composable
+//fun MainPagePreview() {
+//    MainPage()
+//}
+
+
+//@Preview
+//@Composable
+//fun MainBlockPreview() {
+//    MainBlock("main info", "secondary info")
+//}
+
+//@Composable
+//fun MyButton(modifier: Modifier = Modifier) {
+//    Button(onClick = { /*TODO*/ }) {
+//        Row(
+//            verticalAlignment = Alignment.CenterVertically,
+//            horizontalArrangement = Arrangement.SpaceBetween
+//        ) {
+//            Icon(
+//                painter = painterResource(id = R.drawable.statistics),
+//                contentDescription = "",
+//                modifier = modifier
+//                    .size(20.dp))
+//            Text(
+//                text = "top up balance",
+//                fontFamily = FontFamily(
+//                    Font(R.font.montserrat_regular)
+//                ),
+//                modifier = modifier
+//                    .width(100.dp)
+//            )
+//        }
+//
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun MyButtonPreview() {
+//    MyButton()
+//}
+
+@RequiresApi(Q)
 @Composable
-fun MainPagePreview() {
-    MainPage()
+fun MainBlock(mainInfo: String, secondaryInfo: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxWidth()
+            .advancedShadow(
+                color = Color.Black,
+                alpha = 0.1f,
+                cornersRadius = 16.dp,
+                shadowBlurRadius = 80.dp,
+                offsetY = 8.dp,
+            )
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(24.dp) // Указываем радиус скругления
+            )
+            .height(70.dp)
+            .padding(horizontal = paddingStart)
+
+    ) {
+        Text(
+            text = secondaryInfo,
+            style = TextStyle(fontSize = 12.sp, color = Color.LightGray),
+            fontFamily = FontFamily(
+                Font(R.font.montserrat_regular)
+            )
+        )
+        Text(
+            text = mainInfo,
+            style = TextStyle(fontSize = 24.sp, color = Color.Black),
+            fontFamily = FontFamily(
+                Font(R.font.montserrat_medium)
+            )
+        )
+    }
 }
 
+@RequiresApi(Q)
+@Preview
+@Composable
+fun TopUpScreenPreview() {
+    TopUpScreen()
+}
+
+@RequiresApi(Q)
+@Composable
+fun TopUpScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        Text(
+            text = "My Voda",
+            style = TextStyle(
+                fontSize = 16.sp,
+                color = Color.Black,
+            ),
+            fontFamily = FontFamily(
+                Font(R.font.montserrat_bold)
+            ),
+            modifier = modifier
+                .padding(start = paddingStart, top = paddingTop, bottom = 38.dp)
+        )
+        MainBlock(mainInfo = "Main", secondaryInfo = "Secondary")
+    }
+}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun MyNavbar(modifier: Modifier = Modifier) {
+    BottomAppBar (
         modifier = modifier
-    )
+            .height(100.dp)
+    ) {
+        MyNavbarButton(text = "Пополнить баланс", iconId = R.drawable.statistics)
+    }
+}
+
+@Preview
+@Composable
+fun MyNavbarPreview() {
+    MyNavbar()
+}
+
+@Composable
+fun MyNavbarButton(iconId: Int, text: String, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { }) {
+            Icon (
+                painterResource(id = iconId),
+                modifier = modifier.size(20.dp),
+                contentDescription = text,
+            )
+        }
+        Text(text = "Проверить баланс", Modifier.width(73.dp))
+    }
+
+}
+
+@RequiresApi(Q)
+fun Modifier.advancedShadow(
+    color: Color = Color.Black,
+    alpha: Float = 1f,
+    cornersRadius: Dp = 0.dp,
+    shadowBlurRadius: Dp = 0.dp,
+    offsetY: Dp = 0.dp,
+    offsetX: Dp = 0.dp
+) = drawBehind {
+
+    val shadowColor = color.copy(alpha = alpha).toArgb()
+    val transparentColor = color.copy(alpha = 0f).toArgb()
+
+    drawIntoCanvas {
+        val paint = Paint()
+        val frameworkPaint = paint.asFrameworkPaint()
+        frameworkPaint.color = transparentColor
+        frameworkPaint.setShadowLayer(
+            shadowBlurRadius.toPx(),
+            offsetX.toPx(),
+            offsetY.toPx(),
+            shadowColor
+        )
+        it.drawRoundRect(
+            0f,
+            0f,
+            this.size.width,
+            this.size.height,
+            cornersRadius.toPx(),
+            cornersRadius.toPx(),
+            paint
+        )
+    }
 }
---
+++ app/src/main/java/com/example/mynfc/ui/theme/UI.kt
@@ -0,0 +1,6 @@
+package com.example.mynfc.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val paddingStart = 20.dp
+val paddingTop = 24.dp
